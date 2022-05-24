4a9477d3a953cd80b4153396bc9b21327f1b991d
Archie Fan <archiefan@gmail.com>
2022-05-24 10:25:15 +0100

    Add project files.

---
+++ Encapsulation001.Test/Cat.Tests.cs
@@ -0,0 +1,92 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation001.Test
+{
+    [TestFixture]
+    public class CatTests
+    {
+        // Cat initial -> Mood=100, Hungry=100, Energy=100, Meow()
+        // Cat can sleep -> Energy++ Hungry++
+        // Cat can play -> Mood++ Energy-- Meow()
+        // Cat can Feed -> Hungry-- Mood++ Meow()
+
+        [Test]
+        public void Initial_Cat()
+        {
+            
+            // Rarrage
+            
+            // Act
+            Cat cat = new Cat();
+            int Mood = cat.Mood;
+            int Hungry = cat.Hungry;
+            int Energy = cat.Energy;
+
+            // Assert
+            Assert.AreEqual(100, Mood);
+            Assert.AreEqual(100, Hungry);
+            Assert.AreEqual(100, Energy);
+
+        }
+
+        [Test]
+        public void Cat_sleep()
+        {
+            // Act
+            Cat cat = new Cat();
+            var meow = cat.sleep();
+            // Assert
+            Assert.AreEqual(110, cat.Energy);
+            Assert.AreEqual(110, cat.Hungry);
+            Assert.AreEqual("", meow);
+        }
+
+        [Test]
+        public void Cat_play()
+        {
+            // Act
+            Cat cat = new Cat();
+            var meow = cat.play();
+            // Assert
+            Assert.AreEqual(110, cat.Mood);
+            Assert.AreEqual(90, cat.Energy);
+            Assert.AreEqual("Meow~", meow);
+
+        }
+
+
+        [Test]
+        public void Cat_feed()
+        {
+            // Act
+            Cat cat = new Cat();
+            var meow = cat.feed();
+            // Assert
+            Assert.AreEqual(90, cat.Hungry);
+            Assert.AreEqual(110, cat.Mood);
+            Assert.AreEqual("Meow~", meow);
+        }
+
+
+        [Test]
+        public void Cat_play2_feed1_sleep1()
+        {
+            Cat cat = new Cat();
+            cat.play();
+            cat.play();
+            cat.feed();
+            cat.sleep();
+
+            Assert.AreEqual(90, cat.Energy);
+            Assert.AreEqual(130, cat.Mood);
+            Assert.AreEqual(100, cat.Hungry);
+
+        }
+
+    }
+}
---
+++ Encapsulation001/Cat.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation001
+{
+    public class Cat
+    {
+        private int _Mood = 100;
+        private int _Hungry = 100;
+        private int _Energy = 100;
+
+        public int Mood { 
+            get { return _Mood; } 
+        }
+
+        public int Hungry { 
+            get { return _Hungry; } 
+        }
+
+        public int Energy { 
+            get { return _Energy; } 
+        }
+
+        public string sleep()
+        {
+            _Energy = _Energy + 10;
+            _Hungry = _Hungry + 10;
+            return "";
+        }
+
+        public string play()
+        {
+            _Mood = _Mood + 10;
+            _Energy = _Energy - 10;
+            return meow();
+        }
+
+        public string feed()
+        {
+            _Hungry = _Hungry - 10;
+            _Mood = _Mood + 10;
+            return meow();
+        }
+
+        private string meow()
+        {
+            return "Meow~";
+        }
+    }
+}
